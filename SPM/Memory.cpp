12a75d651a6d920b2b728170f831a4b566be4bbe
luan.poi <luan.poi@LAD0327>
2018-11-05 12:17:36 -0200

    memória ajustada.

--- SPM/Memory.cpp
+++ SPM/Memory.cpp
@@ -5,9 +5,6 @@
 
 Memory::Memory()
 {
-	for (WORD cont : inUse) {
-		inUse[cont] = false;
-	}
 }
 
 
@@ -15,41 +12,13 @@ Memory::~Memory()
 {
 }
 
-WORD Memory::getData(WORD position){
-	if (position >= MEMORYSIZE || !inUse[position])
-		return 000;
-	return memorySpace[position][0];
+WORD Memory::read(WORD position){
+	return memorySpace[position];
 }
 
-WORD Memory::setData(WORD data) {
-	while(true){
-		if (pointer >= MEMORYSIZE) {
-			pointer = 0;
-		}
-		else if (!inUse[pointer]) {
-			memorySpace[pointer][0] = data;
-			memorySpace[pointer][1] = 0;
-			inUse[pointer] = true;
-			return pointer;
-		}
-		else if (inUse[pointer] && memorySpace[pointer][1] >= lifetime) {
-			memorySpace[pointer][0] = data;
-			memorySpace[pointer][1] = 0;
-			return pointer;
-		}
-		else {
-			memorySpace[pointer][1] += 1;
-			pointer++;
-		}
-	}	
-}
-
-WORD Memory::setData(WORD data, WORD position) {
-	if (position >= MEMORYSIZE || (inUse[position] && memorySpace[position][1] < lifetime))
-		memorySpace[position][1] += 1;
-		return 000;
-	memorySpace[position][0] = data;
-	memorySpace[position][1] = 0;
-	inUse[position] = true;
-	return data;
+bool Memory::write(WORD data, WORD position) {
+	if (position >= MEMORYSIZE)
+		return false;
+	memorySpace[position] = data;
+	return true;
 }
--- SPM/Memory.h
+++ SPM/Memory.h
@@ -4,21 +4,19 @@
 #define WORDSYSTEMSIZE 64 //32 or 64 bits.
 
 #define MEMORYSIZE (MEMORYSIZEINMEGABYTE*8000000)/WORDSYSTEMSIZE  //8.000.000 (bits) = 1 MegaByte
-typedef unsigned int WORD;
+
+typedef int WORD;
 
 class Memory
 {
 private:
-	WORD memorySpace[MEMORYSIZE][2]; // memorySpace [<Data>] [<lifetime>]. 
-	bool inUse[MEMORYSIZE];
-	WORD pointer;
-	const WORD lifetime = 3;
+	WORD memorySpace[MEMORYSIZE];
+	
 public:
 	Memory();
 	~Memory();
-	WORD getData(WORD position);
-	WORD setData(WORD data);
-	WORD setData(WORD data, WORD position);
+	WORD read(WORD position);
+	bool write(WORD data, WORD position);
 };
 
                                                                                                                                                                                                                                                                                                                                          
